1cb76c105c30e995a5f219f03bd17b93c41cb268
goki22 <goki22.j@gmail.com>
2026-06-15 18:05:24 +0530

    Variabledeclaration

---
+++ variableDec.js
@@ -0,0 +1,19 @@
+let username ="Gokullakshmi jayaraj";
+console.log(username);
+console.log("type of username" + typeof username);
+let organization="CTS";
+console.log(organization);
+console.log("type of organization" + typeof organization);
+let contactnumber = 9876543210
+console.log(contactnumber);
+console.log("type of contactnumber" + typeof contactnumber);
+let isAutomation =true
+console.log(isAutomation);
+console.log("type of isAutomation" + typeof isAutomation);
+let usesplaywright;
+console.log(usesplaywright);
+console.log("type of usesplaywright" + typeof usesplaywright);
+
+
+
+
