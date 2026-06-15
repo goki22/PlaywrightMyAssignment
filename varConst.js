ae9718a45ac0c4a2bfa83c13eea32e5194602e38
goki22 <goki22.j@gmail.com>
2026-06-15 18:31:02 +0530

    Var-cons

---
+++ varConst.js
@@ -0,0 +1,15 @@
+const   browserVersion ="chrome";
+function getBrowserVersion(){
+    if(browserVersion==="chrome")
+        {
+        let  browserVersion= "chrome v130"
+        console.log("Inside block :",browserVersion);
+        }
+        console.log("Outside block:",browserVersion);
+    
+}
+getBrowserVersion(); 
+ 
+
+
+
