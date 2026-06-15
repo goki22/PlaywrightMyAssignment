63f2d50dc266ee5643b2a22f6c8697880c7be081
goki22 <goki22.j@gmail.com>
2026-06-15 21:42:14 +0530

    conditionalStatement

---
+++ conditionalStatement.js
@@ -0,0 +1,13 @@
+function launchBrowser(browserName){
+    if (browserName === "chrome")
+    {
+         console.log("Chrome browser is launched")
+    }
+    else
+    {
+        console.log("Other browser is launched")
+       
+}
+    }
+    launchBrowser ("chrome");
+        
